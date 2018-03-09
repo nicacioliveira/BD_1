52bf968c54dc388f7164b21710cc1729aa9b9bc9
Nicácio Oliveira de Sousa <nnicacio@gmail.com>
2018-03-09 11:24:16 -0300

    atualização de ordem das remoções e nome do arquiv
    Nessa nova ordem, as tabelas podem ser removidas sem que seja necessário adicionar ON DELETE CASCADE nas constraints

---
+++ clean_grupo07.sql
@@ -0,0 +1,12 @@
+
+DELETE FROM cota_area_volume;
+DELETE FROM contribuicao;
+DELETE FROM medicao_cota_diaria;
+DELETE FROM medicao_pluviometrica;
+DELETE FROM posto_pluviometrico;
+DELETE FROM estacao_de_qualidade;
+DELETE FROM acude;
+DELETE FROM rio;
+DELETE FROM bacia;
+DELETE FROM telefones_usuario;
+DELETE FROM usuario;
